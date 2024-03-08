a2712d20b22b6d9a5001e762afedd6ae3df3484b
Pal Kerecsenyi <pal@p.kerecs.com>
2024-03-08 15:09:33 +0000

    Add CORS config

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
-import { Server, Socket } from "socket.io";
-import type { ConfigureRequest, ReadPinRequest, ReadPinResponse, SetPinRequest, SubscribePinRequest, SubscribedPinValue } from "./types";
-import rpio from "rpio";
+import { Server, Socket } from "socket.io"
+import type {
+    ConfigureRequest,
+    ReadPinRequest,
+    ReadPinResponse,
+    SetPinRequest,
+    SubscribePinRequest,
+    SubscribedPinValue,
+} from "./types"
+import rpio from "rpio"
 
 let port = process.env.PORT
 if (!port) {
     port = "8080"
 }
 
-const io = new Server(parseInt(port))
+const io = new Server(parseInt(port), {
+    cors: {
+        origin: "https://esp-frontend.palk.dev",
+        methods: ["GET", "POST"],
+    },
+})
 
 io.on("configurePin", (_: Socket, data: ConfigureRequest) => {
     for (const pin of data.pins) {
         const direction = pin.direction === "input" ? rpio.INPUT : rpio.OUTPUT
 
-        const option = pin.pull === "off" ? rpio.PULL_OFF : pin.pull === "down" ? rpio.PULL_DOWN : pin.pull === "up" ? rpio.PULL_UP : pin.initialValue === true ? rpio.HIGH : pin.initialValue === false ? rpio.LOW : undefined
+        const option =
+            pin.pull === "off"
+                ? rpio.PULL_OFF
+                : pin.pull === "down"
+                  ? rpio.PULL_DOWN
+                  : pin.pull === "up"
+                      ? rpio.PULL_UP
+                      : pin.initialValue === true
+                          ? rpio.HIGH
+                          : pin.initialValue === false
+                              ? rpio.LOW
+                              : undefined
 
         rpio.open(pin.pin, direction, option)
     }
@@ -23,19 +46,22 @@ io.on("setPin", (_: Socket, data: SetPinRequest) => {
     rpio.write(data.pin, data.on ? rpio.HIGH : rpio.LOW)
 })
 
-io.on("readPin", (_: Socket, data: ReadPinRequest, cb: (d: ReadPinResponse) => void) => {
-    const value = rpio.read(data.pin)
-    cb({
-        on: value === rpio.HIGH
-    })
-})
+io.on(
+    "readPin",
+    (_: Socket, data: ReadPinRequest, cb: (d: ReadPinResponse) => void) => {
+        const value = rpio.read(data.pin)
+        cb({
+            on: value === rpio.HIGH,
+        })
+    },
+)
 
 io.on("subscribePin", (s: Socket, data: SubscribePinRequest) => {
     rpio.poll(data.pin, () => {
         const value = rpio.read(data.pin)
         const resp: SubscribedPinValue = {
             pin: data.pin,
-            on: value === rpio.HIGH
+            on: value === rpio.HIGH,
         }
         s.emit("subscribedPinValue", resp)
     })
